729825766f569d271b70423b8ba13ef9c28899f0
Ihwan <mynameisihwan@gmail.com>
2020-03-16 23:37:03 +0700

    add scaffold widget & image widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://pbs.twimg.com/profile_images/698142756128985088/sR34XtdN_400x400.jpg'),
+        ),
+      ),
+    )),
   );
 }
