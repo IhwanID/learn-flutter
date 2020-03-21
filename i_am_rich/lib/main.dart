de1b3ff403fe5c6cda482a721101e0ccdfc935ee
Ihwan <mynameisihwan@gmail.com>
2020-03-21 14:18:29 +0700

    load image from assets

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('I am Rich'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      body: Center(
-        child: Image(
-          image: NetworkImage(
-              'https://pbs.twimg.com/profile_images/698142756128985088/sR34XtdN_400x400.jpg'),
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
         ),
-      ),
-    )),
-  );
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        )),
+  ));
 }
